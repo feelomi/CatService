7405345fd95c10a4a02b6aecebcde728a860246f
feelomi <kvika4733@gmail.com>
2024-11-07 00:25:20 +0200

    Add a Message Bus Publisher to Platform Service

---
+++ PlatformService/Dtos/PlatformPublishedDto.cs
@@ -0,0 +1,9 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformPublishedDto
+    {
+        public int Id { get; set; }
+        public string DogName { get; set; }
+        public string Type { get; set; }
+    }
+}
