d06c21e135589152c54d3ad3de0ec471be22b5ac
feelomi <kvika4733@gmail.com>
2024-11-06 16:32:25 +0200

    first commit

---
+++ Models/Platform.cs
@@ -0,0 +1,11 @@
+namespace PlatformService.Models
+{
+    public class Platform{
+        public int Id { get; set; }
+        public string CatName { get; set; }
+
+        public int Age { get; set; }
+        public string Breed { get; set; }
+        public string Description { get; set; }
+    }
+}
